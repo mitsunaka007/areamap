a41d1e4af87cb6ca8fd6f303ff7788bcc491df14
mitsuka_dev <64319566+mitsunaka007@users.noreply.github.com>
2025-12-18 14:52:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ def index():
         spots=spots
     )
 
-if __name__ == "__main__":
-    app.run(
-        host="127.0.0.1",
-        port=5050,
-        debug=True
-    )
+# if __name__ == "__main__":
+#     app.run(
+#         host="127.0.0.1",
+#         port=5050,
+#         debug=True
+#     )
